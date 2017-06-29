a654f610b56ea9938383fc8d8deca5956bbc79cb
StrongLei <leiq2010@gmail.com>
2017-06-29 11:23:33 +0800

    Create chess.js

---
+++ js/chess.js
@@ -0,0 +1,27 @@
+function pic()
+{
+	var canvas = document.getElementById("testCanvas");
+	var ctx = canvas.getContext("2d");
+	var w = canvas.attributes.width.value;
+	var h = canvas.attributes.height.value;
+	ctx.fillStyle = "rgb(0,0,0)";
+	ctx.fillRect(0, 0, w, h);
+	var imgdata = ctx.getImageData(0, 0, w, h);
+	var pixels = imgdata.data;
+	var i = 0;
+	for (var y = 0; y < h; y++)
+	    for (var x = 0; x < w; x++)
+	    {
+	        //pixels[i++] = x / w * 255;
+	        //pixels[i++] = y / h * 255;
+	        //pixels[i++] = 0;
+	        //pixels[i++] = 255;
+	        var t = (((parseInt(x / 32) % 2) + (parseInt(y / 32) % 2))%2)==1?200:0;
+	        pixels[i++] = t;
+	        pixels[i++] = t;
+	        pixels[i++] = t;
+	        pixels[i++] = 255;
+	                
+	    }
+	ctx.putImageData(imgdata, 0, 0);
+}
